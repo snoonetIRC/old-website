a3e682da632c97a8778fe0d61007b6c2e2b3d8a0
Jeff Sandberg <paradox460@gmail.com>
2012-09-01 16:51:24 -0600

    Heroku retard

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Snoonet
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Speed up asset compilation
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
