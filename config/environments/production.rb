b9d2cdf35959a82270513d35d95af7e294690d60
Jeff Sandberg <paradox460@gmail.com>
2012-12-18 00:24:20 -0800

    Add active admin assets to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,8 @@ Snoonet::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( application-screen.css )
+  # ActiveAdmin dumbness
+  config.assets.precompile += %w{active_admin.css active_admin.js}
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
