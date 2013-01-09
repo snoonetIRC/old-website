70a7c75db41efe77c8bd29cd5996607de4d2d856
Jeff Sandberg <paradox460@gmail.com>
2013-01-08 17:15:35 -0700

    Revert "Added rails console capistrano task"
    This reverts commit 5640a0d848c03c0df2e07ce38b3c2e870602ea09.

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,11 +39,3 @@ namespace :deploy do
   after "deploy:finalize_update", "deploy:symlink_config"
 end
 
-namespace :rails do
-  desc "Open the rails console on one of the remote servers"
-  task :console, :roles => :app do
-    hostname = find_servers_for_task(current_task).first
-    exec "ssh -l #{user} #{hostname} -t 'source ~/.profile && #{current_path}/script/rails c #{rails_env}'"
-  end
-end
-
