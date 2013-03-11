df216cebcf048b12134b2b2f1529754f3e756be1
Jeff Sandberg <paradox460@gmail.com>
2013-03-10 18:32:29 -0700

    Added .nolayout
    Mime alias for html of nolayout
    Added to pages only for now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @page }
+      format.nolayout { render nolayout: @page, layout: 'nolayout', template: 'pages/show.html' }
     end
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Custom Mime types
+# "Nolayout" for HTML without layout
+Mime::Type.register_alias "text/html", :nolayout
+
 # Initialize the rails application
 Snoonet::Application.initialize!
