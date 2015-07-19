d95df821cafacb6b0d5aa621fab63809f2c42040
Sebastian Barfurth <contact@sebastianbarfurth.de>
2015-07-19 23:16:41 +0200

    Style improvements

--- Gemfile
+++ Gemfile
@@ -1,16 +1,11 @@
 ruby '1.9.3'
-
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.11'
 
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
 group :development do
   gem 'sqlite3'
-
   gem 'guard-livereload'
-
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'meta_request'
@@ -19,65 +14,35 @@ end
 gem 'pry'
 gem 'pry-rails'
 gem 'pry-doc'
-
 gem 'rack-throttle', '0.3.0', git: 'git://github.com/karmi/rack-throttle.git', ref: '02297be43714cb990dfe8a582eb5ec90c2861d49'
 
 group :production do
   gem 'pg'
 end
 
-# Gems used only for assets and not required
-# in production environments by default.
 group :assets do
   gem 'sass-rails'
   gem 'coffee-rails'
-
   gem 'jquery-rails'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier'
 end
 
 gem 'compass'
 gem 'compass-rails'
-
 gem 'turbolinks'
-
 gem 'haml'
 gem 'liquid'
 gem 'redcarpet'
-
 gem 'pygments.rb'
-
 gem 'kaminari'
-
 gem 'activeadmin', '~> 0.6.1'
-
-# A non shitty rack server
+gem 'ancestry'
 gem 'puma'
 
-# Use Capistrano for deployment
 group :development do
-  gem 'capistrano',         require: false
-  gem 'capistrano-rbenv',   require: false
-  gem 'capistrano-rails',   require: false
-  gem 'capistrano-bundler', require: false
-  gem 'capistrano3-puma',   require: false
+  gem 'capistrano'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-rails'
+  gem 'capistrano-bundler'
+  gem 'capistrano3-puma'
 end
-
-# Sidebar tree
-gem 'ancestry'
-
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# To use debugger
-# gem 'debugger'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,27 @@
 server 'snoonet.org', port: 6969, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@github.com:snoonetIRC/website.git'
-set :application,     'website'
-set :user,            'deployer'
-set :rbenv_type,      :user
-set :rbenv_ruby,      '1.9.3-p551'
-set :puma_threads,    [4, 16]
-set :puma_workers,    1
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :repo_url, 'git@github.com:snoonetIRC/website.git'
+set :application, 'website'
+set :user, 'deployer'
+set :rbenv_ruby, '1.9.3-p551'
+set :rbenv_type, :user
+set :puma_threads, [4, 16]
+set :puma_workers, 1
+set :pty, true
+set :use_sudo, false
+set :stage, :production
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :linked_files, %w{config/database.yml .rbenv-vars}
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
-
-## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml .rbenv-vars}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -70,7 +67,3 @@ namespace :deploy do
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
