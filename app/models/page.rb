ddc1f20d79b0dcbec6f9bb4c98a3ec3bad2e304b
Ryan Scott Lewis <ryan@rynet.us>
2016-06-29 06:03:53 -0400

    Update Page model: parameterize permalink and update content if needed on save, add markdown checksum check, and override #to_param to return permalink for use with view helpers

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,28 @@
 class Page < ActiveRecord::Base
-  enum content_type: [ :html ]
+
+  before_save :parameterize_permalink
+  before_save :update_content_if_needed
+
+  # Needed to make `redirect_to @page` and other helpers work correctly
+  def to_param
+    permalink
+  end
+
+  protected
+
+  def parameterize_permalink
+    self.permalink = name if permalink.blank?
+
+    self.permalink = permalink.parameterize
+  end
+
+  def update_content_if_needed
+    checksum = Digest::MD5.hexdigest(content_markdown)
+
+    if content_checksum != checksum # content_markdown has changed.
+      self.content_checksum = checksum
+      self.content_html = MARKDOWN_RENDERER.render(content_markdown)
+    end
+  end
+
 end
