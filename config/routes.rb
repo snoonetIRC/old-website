8a07f9ab6c469a5a60913bd4aa881ac0eca644a2
Ryan Scott Lewis <ryan@rynet.us>
2016-06-29 06:01:17 -0400

    Fix routing issues for pages (/sitemap, /pages, /*permalink)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'errors/show'
+
+  # get 'errors/show'
 
   %w[404 422 500 503].each do |code|
     get code, to: "errors#show", code: code.to_i
@@ -7,6 +8,15 @@ Rails.application.routes.draw do
 
   root "pages#root"
 
-  get "/sitemap",   to: "pages#index"
-  get "*permalink", to: "pages#show"
+  get  "/sitemap",   to: "pages#index",  as: :pages
+  post "/pages",     to: "pages#create", as: :create_page
+  get  "/pages/new", to: "pages#new",    as: :new_page
+
+  # NOTE: The follow must be at the end of the list of routes.
+  get    "*permalink",      to: "pages#show",  as: :page
+  get    "*permalink/edit", to: "pages#edit",  as: :edit_page
+  patch  "*permalink",      to: "pages#update"
+  put    "*permalink",      to: "pages#update"
+  delete "*permalink",      to: "pages#delete"
+
 end
