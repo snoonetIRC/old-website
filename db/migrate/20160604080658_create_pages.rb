775689e499901e71b8dc0a3c9625e5762552e4f8
Ryan Scott Lewis <ryan@rynet.us>
2016-06-29 06:00:12 -0400

    Update create_pages DB migration

--- db/migrate/20160604080658_create_pages.rb
+++ db/migrate/20160604080658_create_pages.rb
@@ -1,10 +1,11 @@
 class CreatePages < ActiveRecord::Migration
   def change
     create_table :pages do |t|
-      t.string :name, null: false
-      t.string :permalink, null: false
-      t.string :content, null: false
-      t.integer :content_type, null: false, default: 0
+      t.string :name,             null: false
+      t.string :permalink,        null: false
+      t.string :content_markdown, null: false
+      t.string :content_html,     null: false
+      t.string :content_checksum, null: false
 
       t.timestamps null: false
     end
