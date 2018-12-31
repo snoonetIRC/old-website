1c50184d01764bc3b54e8477019875f36252048c
linuxdaemon <linuxdaemon@snoonet.org>
2018-12-31 04:31:10 -0600

    Move base secret_token to an environment var
    Also update the token on the server since it has been in the public repo
    for a while

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Snoonet::Application.config.secret_token = '983edef0276001bd6acdb248706345bf5bc997ba99a930c17db17d1fbfc3d12fdb41ca7402a401cbe4c7387c98ad583c5ccf77d8e04a66b0f5b926fd2fd0669c'
+Snoonet::Application.config.secret_token = ENV["SECRET_KEY_BASE"]
